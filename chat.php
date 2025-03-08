c4b408e80f2d4eae432e4f6ffd7c6f7a86eeb939
Jake Brooks <104141188+jakebrooks342@users.noreply.github.com>
2025-03-08 14:22:28 +0000

    Basic skeleton for site
    Need to install either php or XAMPP to run site

---
+++ chat.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Text Chat Page</h1>";
+echo "<p>Welcome to the chat section of Made It All.</p>";
+?>
---
+++ dataAnalytics.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Data Analytics Page</h1>";
+echo "<p>Welcome to the data analytics section of Made It All.</p>";
+?>
