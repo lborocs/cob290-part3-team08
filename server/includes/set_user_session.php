9e0c16986048a617055a3db38c3bae587bef911b
liamPort <liamparker100@outlook.com>
2025-05-01 16:42:39 +0100

    added very basic api to get tasks from the sever via session vars, pageId, pageType

--- server/includes/set_user_session.php
+++ server/includes/set_user_session.php
@@ -4,6 +4,9 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['user_id'])) {
     $_SESSION['user_id'] = $_POST['user_id'];
+    //jeven u can change v ==========================================================================================================
+    $_SESSION['page_id'] = "1";
+    $_SESSION['page_type'] = "project";
     header("Location:  index.php");
     exit;
 }
